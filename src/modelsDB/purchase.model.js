76e10c6ea975363bb57b32c347ba334d83d4b35d
Reefleexx <dzemichivan@gmail.com>
2022-04-07 02:45:27 +0300

    update product sizes done

--- src/modelsDB/purchase.model.js
+++ src/modelsDB/purchase.model.js
@@ -68,29 +68,29 @@ purchaseSchema.statics.updateUser = async function(id, userId, type) {
    await user.save({validateBeforeSave: false})
 }
 
-
-// Get total price and amount of products
+// 1) Get total price and amount of products and check product availability
 purchaseSchema.pre('save', async function(next) {
    const user = await User.exists({user: this.user})
 
-   // Embed price field and await it all
+   // 1) Embed price field and await it
    const products = await Promise.all(this.products.map(async el => {
-      const product = await Product.findById(el.id).select('price sizes').lean()
-
-      // Check if it is enough amount
-      if (product.sizes)
+      const product = await Product.findById(el.id).select('price numSizes').lean()
 
       // If no product return undefined
       if (!product) return undefined
 
+      // Check if it is enough amount
+      if (product.numSizes[el.size] < el.amount)
+         return next(new AppError('This sizes amount is not available'))
+
       return {price: product.price, amount: el.amount}
    }))
 
-   // Check if such product and user exists
+   // 2) Check if such product and user exists
    if (products.includes(undefined) || !user)
       return next(new AppError('User or product id is deprecated or invalid'))
 
-   // Calc totals
+   // 3) Calc totals
    const totals = products.reduce((acc, el) => {
       acc.price = acc.price + el.price * el.amount
       acc.amount = acc.amount + el.amount
@@ -98,16 +98,80 @@ purchaseSchema.pre('save', async function(next) {
       return acc
    }, {price: 0, amount: 0})
 
-   // Save it all
+   // 4) Save it all
    this.totalPrice = totals.price
    this.totalAmount = totals.amount
 
    next()
 })
 
-// Update relative collections
+// 1) Update user collection (minus sizes)
+purchaseSchema.pre('save', async function(next) {
+   // 1) Get purchases products ids
+   const productsIds = this.products.reduce((acc, el) => {
+      const is = acc.find(val => val.toString() === el.id.toString())
+      if (is) {} else {
+         acc.push(el.id.toString())
+      }
+      return acc
+   }, [])
+
+      // Get products
+   const products = await Product.find({_id: {$in: productsIds}}).lean()
+
+   // 2) Update Products
+   const updateProduct = async () => {
+      for (const i in productsIds) {
+         // 1) Get id values and amount values
+         const productId = productsIds[i]
+         const productObjValues = this.products.filter(val => val.id.toString() === productId.toString())
+
+         // 2) If same products
+         let curSizes = productObjValues.length > 1 ?
+            products.find(val => val._id.toString() === productObjValues[i].id.toString()).numSizes
+            : products.find(val => val._id.toString() === productObjValues[0].id.toString()).numSizes
+
+            // Create new sizes
+         let newSizes = {...curSizes}
+
+         const reCalcAndSave = async () => {
+            for (const n in productObjValues) {
+               const currentObj = productObjValues[n]
+
+               // Update sizes
+               const newVal = newSizes[currentObj.size] - currentObj.amount
+               newSizes =  {
+                  ...newSizes,
+                  [currentObj.size]: newVal
+               }
+
+               // 3) Update current product
+               const response = await Product.findByIdAndUpdate(
+                  productId, {numSizes: newSizes}
+               )
+            }
+         }
+
+         await reCalcAndSave()
+      }
+   }
+
+   await updateProduct()
+
+   next()
+})
+
+// 2) Update relative collections
 purchaseSchema.post('save', async function() {
    await this.constructor.updateUser(this._id, this.user, 'save')
+})
+
+// 3) On status = success update product
+purchaseSchema.post(/update/i, async function(doc) {
+   console.log(doc)
+   if (this._update['$set'].status === 'canceled') {
+      console.log('hiiiiii')
+   }
 })
 
 module.exports = model('Purchase', purchaseSchema)
