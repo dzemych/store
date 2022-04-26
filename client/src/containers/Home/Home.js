dc982d2daff901f99419e83c3e5634efc0c96970
Reefleexx <dzemichivan@gmail.com>
2022-04-26 22:02:20 +0300

    working on home

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import classes from './Home.module.sass'
+import {NavLink} from "react-router-dom";
+import {ReactComponent as FashionAndStyle} from '../../img/fashionAndStyle.svg'
 
 
 const Home = (props) => {
     return (
        <div className={classes.container}>
-          <h1>Home page</h1>
+          <div className={classes.wrapper}>
+             <section className={classes.preview}>
+                <nav className={classes.preview_nav}>
+                  <NavLink to={'/info'}>Info</NavLink>
+                  <NavLink
+                     to={'/contacts'}
+                     className={classes.nav_contact}
+                  >Contacts</NavLink>
+                  <NavLink to={'/about'}>About</NavLink>
+                </nav>
+
+                <FashionAndStyle className={classes.preview_img}/>
+
+                <button className={classes.btnCatalog}>
+                  Catalog
+                </button>
+             </section>
+          </div>
        </div>
     )
 }
