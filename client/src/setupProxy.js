f31aeae2b3238feff985b88ae073ab799c0b903e
dzemych <dzemichivan@gmail.com>
2022-06-26 19:24:03 +0300

    coop bug

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
    app.use(
       '/api',
       createProxyMiddleware({
-         target: "http://localhost:5000",
+         target: "http://tandem:80",
          changeOrigin: true,
       })
    );
